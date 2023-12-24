393d978591a8b09d6bd2748e952bbbf0929170f5
Michael <michaelk99@gmail.com>
2023-12-24 00:49:02 -0800

    Add priority menu to note and cards.

--- src/Card.js
+++ src/Card.js
@@ -18,21 +18,14 @@ export class Card {
       noteCard.classList.add(`${project.name}`)
       console.log(note.displayed);
       note.displayed = true
-    // const projectList = noteCard.querySelector(".note-projects")
-    // console.log(projectList);
-    // const select = noteCard.querySelector("select")
-    // console.log(select);
-    //   select.remove()
-    // const projectSpan = document.createElement("span")
-    //   projectSpan.type = input
-    //   projectList.appendChild(projectSpan)
-    //   console.log(projectSpan);
+   
     const getPostedDateSpan = noteCard.querySelector(".posted-date")
       getPostedDateSpan.textContent = `${note.date}`
     const cardInputs = Array.from(noteCard.getElementsByClassName("note-form"))
     const noteValues = Array.from(note.note.values())
       cardInputs.forEach((input, i) => {
         console.log(noteValues[i]);
+        console.log(input);
         console.log(input.classList);
         if (input.classList.contains("note-projects")) {
           console.log("SELECT=====");
@@ -42,6 +35,18 @@ export class Card {
           const projectSpan = document.createElement("span")
           projectSpan.textContent = noteValues[i]
           projectList.appendChild(projectSpan)
+        }
+        if (noteValues[i] === "low" || noteValues[i] === "medium" || noteValues[i] === "high") {
+          console.log("PRIORITY ============");
+          console.log(noteValues);
+          this.borderColor(noteValues[i], noteCard)
+          const selectMenu = noteCard.querySelector(".note-priority")
+          console.log(selectMenu.options);
+          for(const option of selectMenu) {
+            if (option.value === noteValues[i]) {
+              option.setAttribute("selected", true)
+            }
+          }
         }
         input.value = noteValues[i]
       })
@@ -62,6 +67,20 @@ export class Card {
               currentNoteValues.forEach(input => {
                 let oldNoteValue = note.note.get(`${input.name}`)
                 if (input.value != oldNoteValue) {
+                  console.log(input.value);
+                  console.log(note.note);
+                  if (input.value === "low" || input.value === "medium" || input.value === "high") {
+                    console.log("PRIORITY ============");
+                    // console.log(noteValues);
+                    this.borderColor(input.value, noteCard)
+                    const selectMenu = noteCard.querySelector(".note-priority")
+                    console.log(selectMenu.options);
+                    for(const option of selectMenu) {
+                      if (option.value === input.value) {
+                        option.setAttribute("selected", true)
+                      }
+                    }
+                  }
                   note.note.set(`${input.name}`, input.value)
                 }
             })
@@ -80,5 +99,19 @@ export class Card {
           projectNotes.splice(i, 1)
           noteCard.remove()
         })
+      }
+      borderColor(val, card) {
+        switch (val) {
+          case "low":
+            card.style.borderColor = "var(--teal-3)"
+            break;
+          case "medium":
+            card.style.borderColor = "var(--yellow-3)"
+            break;
+          case "high":
+            card.style.borderColor = "var(--red-3)"
+          default:
+            break;
+        }
       }
 }
--- src/Note.js
+++ src/Note.js
@@ -5,9 +5,24 @@ export default class Note {
   constructor(note, name) {
     // super()
     this.note = new FormData(note),
+    this.priority = this._priority,
     this.id = counter++,
     this.date = this._date,
     this.displayed = false
+  }
+  get priority() {
+    return this._priority
+  }
+  set priority(val) {
+    console.log(val);
+    const priorityOption = document.querySelector(".note-priority")
+    console.log(priorityOption.option);
+    // priorityOption.selectedOptions.filter(selected => {
+    //   console.log(selected);
+    // })
+    val = priorityOption.value
+    // val.setAttribute("selected")
+    return this._priority = val
   }
   get date() {
     return this._date
